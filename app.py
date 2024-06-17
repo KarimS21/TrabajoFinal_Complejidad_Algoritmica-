917336d333c3642d5fbc5abced9526e24b668b35
Sr-Anonymus-maker <66220812+Sr-Anonymus-maker@users.noreply.github.com>
2024-06-16 21:41:11 -0500

    Revert "search-integration"

--- app.py
+++ app.py
@@ -1,25 +1,44 @@
-from flask import Flask, render_template, request, redirect, url_for
-
+from flask import Flask, redirect, url_for, render_template, request, session
+from BuscadorBFS import buscar_por_nombre_o_apellido, encontrar_mejor_coincidencia, data
 app = Flask(__name__)
+app.secret_key = "hello"
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method == "POST":
+        usuario_id = request.form["id_user"]
 
-@app.route('/results', methods=['POST'])
-def results():
-    user_id = request.form['user-id']
-    user_name = request.form['user-name']
-    # Aquí puedes llamar a la función del algoritmo con user_id y user_name
-    # resultado = tu_funcion_del_algoritmo(user_id, user_name)
-    # Simulando un resultado
-    resultado = {
-        'datos': f'ID de usuario: {user_id}, Nombre: {user_name}',
-        'persona': 'Ejemplo Persona',
-        'amigos_en_comun': 5,
-        'camino': 'Camino Ejemplo'
-    }
-    return render_template('results.html', resultado=resultado)
+        nombre_o_apellido = request.form["nombre_Apellido"]
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        resultados = buscar_por_nombre_o_apellido(nombre_o_apellido)
+        ##hasta aqui deberia funcionar todo bien,
+        #de aqui para abajo se debe pensar como representar el resultado en el html
+        #para ver que todo funciona, miren la terminal, ahi sale el resultado del algoritmo
+        
+        if resultados.empty:
+            print("No se encontraron resultados.")
+        else:
+            print("Resultados encontrados:")
+            print(resultados[['ID', 'Nombre', 'Apellido']])
+            posibles_ids = resultados['ID'].astype(str).tolist()
+    
+            mejor_coincidencia, num_amigos_comun, mejor_camino = encontrar_mejor_coincidencia(usuario_id, posibles_ids)
+    
+        mejor_persona = data[data['ID'] == int(mejor_coincidencia)]
+        print("\nLa persona con más amigos en común es:")
+        print(mejor_persona[['ID', 'Nombre', 'Apellido']])
+        print(f"Cantidad de amigos en común: {num_amigos_comun}")
+        print(f"Camino BFS: {mejor_camino}")
+
+
+
+
+
+
+
+        return render_template("index.html")
+    
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080,debug=True)
