9a8b618c79294fdb4c8b5d4d42d5feedf525999c
KarimS21 <U201816862@upc.edu.pe>
2024-06-22 17:58:19 -0500

    rebase:Se cambio el dataset de usuarios, y se cambiaron algunos estilos del diseño

--- app.py
+++ app.py
@@ -27,21 +27,25 @@ def results():
     for _ in range(3):  # Intentar obtener hasta 3 mejores coincidencias
         mejor_coincidencia, max_amigos_comun, mejor_camino = encontrar_mejor_coincidencia(user_id, posibles_ids)
         if mejor_coincidencia:
-            # Obtener el nombre y apellido del usuario encontrado
+            # Obtener el nombre y apellido del usuario encontrado 
             usuario = data[data['ID'] == int(mejor_coincidencia)].iloc[0]
             nombre_completo = f"{usuario['Nombre']} {usuario['Apellido']}"
-            coincidencias.append((mejor_coincidencia, nombre_completo, max_amigos_comun, mejor_camino))
+            edad = usuario["Edad"]
+            gusto = usuario["Gusto"]
+            coincidencias.append((mejor_coincidencia, nombre_completo,edad,gusto, max_amigos_comun, mejor_camino))
             posibles_ids.remove(mejor_coincidencia)  # Remover la mejor coincidencia encontrada para la próxima iteración
         else:
             break  # Si no se encuentran más coincidencias, salir del bucle
     
     resultados = []
     for coincidencia in coincidencias:
-        mejor_coincidencia, nombre_completo, max_amigos_comun, mejor_camino = coincidencia
+        mejor_coincidencia, nombre_completo,edad,gusto, max_amigos_comun, mejor_camino = coincidencia
         resultado = {
             'persona': nombre_completo if mejor_coincidencia else 'No encontrada',
             'id': mejor_coincidencia if mejor_coincidencia else 'N/A',
             'amigos_en_comun': max_amigos_comun if mejor_coincidencia else 'N/A',
+            'edad': edad if mejor_coincidencia else 'N/A',
+            'gusto': gusto if mejor_coincidencia else 'N/A',
             'camino': ' -> '.join(mejor_camino) if mejor_camino else 'N/A'
         }
         resultados.append(resultado)
